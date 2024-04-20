6403d7a8ab770406dc5549d759c0fe5551a8c28e
Paweł Nurzyński <pawel.nurzynski@comarch.com>
2024-04-20 15:52:59 +0200

    Initial commit

---
+++ api/src/main/java/com/pawelnu/reactandhtmx/ReactandhtmxApplication.java
@@ -0,0 +1,13 @@
+package com.pawelnu.reactandhtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactandhtmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactandhtmxApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/pawelnu/reactandhtmx/ReactandhtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawelnu.reactandhtmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactandhtmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
